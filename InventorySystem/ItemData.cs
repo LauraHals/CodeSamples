22151cc5e54394ff36aadade0eaa9cc71958926c
Laura Halsinaho <47883447+LauraHals@users.noreply.github.com>
2021-12-31 15:42:33 +0200

    Add files via upload

--- InventorySystem/ItemData.cs
+++ InventorySystem/ItemData.cs
@@ -2,19 +2,19 @@
 
 public abstract class ItemData : ScriptableObject
 {
-	public string itemName;
-	[TextArea(1, 4)]
-	public string description;
-	public int cost;
-	[Tooltip("Required level to unlock item")]
-	public int playerLevel;
-	public Sprite icon;
-	[Tooltip("Remove item after using")]
-	public bool singleUse = true;
-	[Tooltip("Item duration or 'item used'-indicator duration ")]
-	[Range(0.1f, 999f)]
-	public float duration = 0.5f;
+    public string itemName;
+    [TextArea(1, 4)]
+    public string description;
+    public int cost;
+    [Tooltip("Required level to unlock item")]
+    public int playerLevel;
+    public Sprite icon;
+    [Tooltip("Remove item after using")]
+    public bool singleUse = true;
+    [Tooltip("Item duration or 'item used'-indicator duration ")]
+    [Range(0.1f, 999f)]
+    public float duration = 0.5f;
 
-	public abstract void Use();
+    public abstract void Use();
 
 }
--- InventorySystem/ItemSlot.cs
+++ InventorySystem/ItemSlot.cs
@@ -3,20 +3,20 @@ using UnityEngine.UI;
 
 public abstract class ItemSlot : MonoBehaviour
 {
-	[SerializeField]
-	private Image icon;
+    [SerializeField]
+    private Image icon;
 
-	protected ItemData itemData;
+    protected ItemData itemData;
 
-	/// <summary>
-	/// Assigns correct icon and data to slot-button
-	/// </summary>
-	/// <param name="itemData">ItemData in slot</param>
-	public virtual void InitializeSlot(ItemData itemData)
-	{
-		this.itemData = itemData;
-		icon.sprite = itemData.icon;
-	}
+    /// <summary>
+    /// Assigns correct icon and data to slot-button
+    /// </summary>
+    /// <param name="itemData">ItemData in slot</param>
+    public virtual void InitializeSlot(ItemData itemData)
+    {
+        this.itemData = itemData;
+        icon.sprite = itemData.icon;
+    }
 
-	public abstract void OnClick();
+    public abstract void OnClick();
 }
