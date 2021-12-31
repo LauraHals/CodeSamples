711ae2163d1ec3f3f3da4308e75531a49e8fa699
Laura Halsinaho <47883447+LauraHals@users.noreply.github.com>
2021-12-31 15:43:45 +0200

    Update ShopItemSlot.cs

--- InventorySystem/Shop/ShopItemSlot.cs
+++ InventorySystem/Shop/ShopItemSlot.cs
@@ -1,8 +1,3 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
 public class ShopItemSlot : ItemSlot
 {
     public override void OnClick()
