9067463697b4ce6c1ed61c9e824df134ff1dbe60
Laura Halsinaho <47883447+LauraHals@users.noreply.github.com>
2021-12-31 15:35:31 +0200

    Add files via upload

---
+++ InventorySystem/Shop/ShopData.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+//Used to quickly switch between shop-presets
+[CreateAssetMenu(menuName = "FF-Assets/Shop")]
+public class ShopData : ScriptableObject
+{
+    public ItemData[] itemsInShop;
+}
---
+++ InventorySystem/Shop/ShopItemSlot.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopItemSlot : ItemSlot
+{
+	public override void OnClick()
+	{
+		if (UIhandler.Instance != null)
+		{
+			UIhandler.Instance.TryBuyItem(itemData);
+		}
+	}
+}
