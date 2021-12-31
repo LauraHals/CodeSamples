316263a9011c3d29453654b26340108f479be172
Laura Halsinaho <47883447+LauraHals@users.noreply.github.com>
2021-12-31 15:42:58 +0200

    Add files via upload

--- InventorySystem/Shop/ShopItemSlot.cs
+++ InventorySystem/Shop/ShopItemSlot.cs
@@ -5,11 +5,11 @@ using UnityEngine.UI;
 
 public class ShopItemSlot : ItemSlot
 {
-	public override void OnClick()
-	{
-		if (UIhandler.Instance != null)
-		{
-			UIhandler.Instance.TryBuyItem(itemData);
-		}
-	}
+    public override void OnClick()
+    {
+        if (UIhandler.Instance != null)
+        {
+            UIhandler.Instance.TryBuyItem(itemData);
+        }
+    }
 }
