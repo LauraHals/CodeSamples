c921cebd6447f9112113810e4d6dd8ec0cec0a12
Laura Halsinaho <47883447+LauraHals@users.noreply.github.com>
2021-12-31 15:44:48 +0200

    Update InventoryItemSlot.cs

--- InventorySystem/InventoryItemSlot.cs
+++ InventorySystem/InventoryItemSlot.cs
@@ -1,9 +1,7 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using static GameSystems;
 
 public class InventoryItemSlot : ItemSlot
 {
